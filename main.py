f81da95077890a648ae9ceab95a375da2b6711ae
TobiasContrera <tobiascontrera10@gmail.com>
2024-01-19 16:24:06 -0300

    primer Guardado

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hola mundo")
+
+lista = ["Argentina","Noruega","España","EE UU"]
+
+for x in lista:
+    print(x)
+
+
+
+# 1°: git init para comenzar a guardar un archivo en la terminal
+# 2|: El archivo se guardara como .git (Archivo oculto), se coloca "dir /a"para abrirlo
+    
