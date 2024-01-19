c0073336e6e091711e2f288b3de4867d6c53e0f3
TobiasContrera <tobiascontrera10@gmail.com>
2024-01-19 16:33:00 -0300

    segunda modificacion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("hola mundo")
 
-lista = ["Argentina","Noruega","España","EE UU"]
+lista = ["Argentina","Noruega","España","EE UU","Suezia","Inglaterra"]
 
 for x in lista:
     print(x)
@@ -8,5 +8,9 @@ for x in lista:
 
 
 # 1°: git init para comenzar a guardar un archivo en la terminal
-# 2|: El archivo se guardara como .git (Archivo oculto), se coloca "dir /a"para abrirlo
-    
+# 2°: El archivo se guardara como .git (Archivo oculto), se coloca "dir /a"para abrirlo
+# 3°: git status te muestra el estado de tus archivos
+# 4°: git add . para versionar (guardar) todos los archivos 
+# 5° : git log para ver los commit 
+# 6° : git commit -m "" para guardar comentarios
+# 7° : git log --oneline para ver el resumen de los coment     
---
+++ zip.py
@@ -0,0 +1,6 @@
+
+nombres = ["Marcos","nico","Lucas"]
+apellidos = ["Gonzales","Amarto","Sosa"]
+
+for n, a in zip (nombres, apellidos):
+    print (f"{a}, {n}")
